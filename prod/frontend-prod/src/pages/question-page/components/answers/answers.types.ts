9c311fd21fa00078b96b08fa0562dc37f6fe3fdc
Sergi Papinashvili <papinashvilisergi@gmail.com>
2024-12-05 07:57:41 +0000

    ok

---
+++ prod/frontend-prod/src/pages/question-page/components/answers/answers.types.ts
@@ -0,0 +1,16 @@
+export type Answer = {
+  id: number;
+  text: string;
+  likes_count: number;
+  is_correct: boolean;
+  author: {
+    id: number;
+    fullname: string;
+    email: string;
+    rating: number;
+  };
+};
+
+export type AnswersProps = {
+  answers: Answer[];
+};
---
+++ prod/frontend-prod/src/types/interfaces.ts
@@ -0,0 +1,32 @@
+export interface Answer {
+  id: string;
+  questionId: string;
+  description: string;
+  author: string;
+  isCorrect: boolean;
+  date: string;
+}
+export interface Question {
+  id: string;
+  author: Author;
+  title: string;
+  description: string;
+  tag_names: string[];
+  created_at: string;
+  answers: Answer[];
+}
+export interface Author {
+  id: string;
+  fullname: string;
+  email: string;
+  rating: number;
+  questions: Question[];
+}
+export interface User {
+  id: string;
+  fullname: string;
+  email: string;
+  rating: number;
+  questions: Question[];
+  answers: Answer[];
+}
