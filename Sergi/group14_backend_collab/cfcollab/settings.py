d5e392c56b3c8ac9d50c3f4f3dcf664a5e56e78e
Sergi Papinashvili <papinashvilisergi@gmail.com>
2024-12-08 12:58:26 +0000

    beqi-mushaobs

--- Sergi/group14_backend_collab/cfcollab/settings.py
+++ Sergi/group14_backend_collab/cfcollab/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files for user-uploaded files
 MEDIA_URL = '/media/'
